4c6b65aa08651bdbc317c9d157c4cf86de157d2d
Aram Avila-Herrera <Aram.Avila-Herrera@ucsf.edu>
2013-11-14 11:58:03 -0800

    default __SRC_PATH set with ${HOME}

--- runInfCalc.sh
+++ runInfCalc.sh
@@ -3,7 +3,7 @@
 # Email: Aram.Avila-Herrera@ucsf.edu
 
 #export __SRC_PATH="/path/to/infstats"
-export __SRC_PATH="/home/aram/dev-src/infstats_calc"
+export __SRC_PATH="${HOME}/dev-src/infstats_calc"
 
 python ${__SRC_PATH}/infCalc.py ${@}
 
